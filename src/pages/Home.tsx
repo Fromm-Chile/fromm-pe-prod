a39a5aaa309d881af3c997c82463437f4737608d
David Guzman <davidguzman@Rexs-MacBook-Pro.local>
2024-11-04 21:05:27 -0300

    cloudflare deployment test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 export const Home = () => {
   return (
     <div className='flex justify-center items-center h-[80vh] text-5xl'>
-      <p>HOME</p>
+      <p>PRUEBA DE DESPLIEGUE CON CLOUDFLARE</p>
     </div>
   )
 }
