3695878802fc598c486533b76bcb47fac3514832
David Guzman <davidguzman1500@gmail.com>
2025-01-27 18:57:52 -0300

    refactor: enhance loader component with spinner animation and logo

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,17 @@
 export const Loader = () => {
   return (
-    <div className="h-[70vh] flex justify-center items-center text-4xl font-semibold text-red">
-      Loading...
+    <div className="h-[70vh] flex justify-center items-center text-4xl font-semibold text-red relative">
+      <div
+        className="inline-block h-60 w-60 animate-spin rounded-full border-[10px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+      <div className="absolute">
+        <img src="/img/Navbar/FrommLogo.webp" height={190} width={190} />
+      </div>
     </div>
   );
 };
