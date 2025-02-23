7b4019b08a51db3303c0534b89c9de1f0a4a6c5e
David Guzman <davidguzman1500@gmail.com>
2025-02-22 23:24:09 -0300

    fix: increase height of image container in InfoProductCard for better layout

--- src/components/InfoProductCard.tsx
+++ src/components/InfoProductCard.tsx
@@ -18,7 +18,7 @@ export const InfoProductCard = ({
   return (
     <Link to={link}>
       <div className="bg-white px-5 py-8 my-5 h-[450px] flex flex-col justify-between rounded-md hover:shadow-2xl hover:border-2 hover:border-red transition-all ease duration-500 border border-primaryGray">
-        <div className="md:h-[250px] flex items-center">
+        <div className="md:h-[340px] flex items-center">
           <img src={srcImg} alt={alt} />
         </div>
         <div>
